518a31327fe447fd61454455040f7145f757c4c5
Sherkhan K <sherkhan4work@gmail.com>
2024-01-10 19:55:22 +0600

    logo changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom";
-import { I18nextProvider } from "react-i18next";
+// import { I18nextProvider } from "react-i18next";
 import "antd/dist/antd.css";
 
 import Router from "./router";
-import i18n from "./translation";
+// import i18n from "./translation";
 
 const App = () => (
   <BrowserRouter>
-    <I18nextProvider i18n={i18n}>
-      <Router />
-    </I18nextProvider>
+    {/* <I18nextProvider i18n={i18n}> */}
+      {/* <Router/>  */}
+      <h1>Hello World!</h1>
+      <h2>This website soon will be made by Sherkhan K.</h2>
+    {/* </I18nextProvider> */}
   </BrowserRouter>
 );
 
