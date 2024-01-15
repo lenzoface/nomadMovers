f729c8793738ad1d19e54cef3939c49dc5096b22
Sherkhan K <sherkhan4work@gmail.com>
2024-01-15 23:33:00 +0600

    back to normal routing

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,13 @@ import ReactDOM from "react-dom";
 // import { I18nextProvider } from "react-i18next";
 import "antd/dist/antd.css";
 
-// import Router from "./router";
+import Router from "./router";
 // import i18n from "./translation";
 
 const App = () => (
   <BrowserRouter>
     {/* <I18nextProvider i18n={i18n}> */}
-      {/* <Router/>  */}
-      <h1>Hello World!</h1>
-      <h2>This website soon will be made by Sherkhan K.</h2>
+      <Router/> 
     {/* </I18nextProvider> */}
   </BrowserRouter>
 );
